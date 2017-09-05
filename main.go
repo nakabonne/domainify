c257dc95973ab852b51d059c0552f063dde12f76
ryonakao <rodriguez.nak@gmail.com>
2017-09-05 10:57:45 +0900

    create seed

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
 var tlds = []string{"com", "net"}
 
 const allowedChars = "abcdefghijklmnopqrstuvwxyz0123456789_-"
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
